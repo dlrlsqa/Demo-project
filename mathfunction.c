4898e7351e9a9e62a0a9260e7780e64a6c203dbb
dlrlsqa <100190272+dlrlsqa@users.noreply.github.com>
2022-02-23 09:54:03 +0530

    Update mathfunction.c
    vvv

--- mathfunction.c
+++ mathfunction.c
@@ -2,10 +2,8 @@
 
 int add (int firstNumber, int secondNumber)
 {
-    return firstNumber + secondNumber
 }
 
 int subtract (int firstNumber, int secondNumber)
 {
-    return firstNumber + secondNumber
 }
