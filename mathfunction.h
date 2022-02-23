76969563ae77d08fcf5728a0dac0143348ceec5b
dlrlsqa <100190272+dlrlsqa@users.noreply.github.com>
2022-02-23 09:56:56 +0530

    Update mathfunction.h

--- mathfunction.h
+++ mathfunction.h
@@ -1,2 +1,3 @@
 int add(int firstNumber, int secondNumber);
-int subtract(int firstnumber, int secondNumber);
+int subtract(int firstnumber, int secondNumber);int subtract(int firstnumber, int secondNumber);
+int multiply(int firstnumber, int secondNumber);
